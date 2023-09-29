2e0e123d5147b83f212ba18e13d8311fa0b23d2c
kaleem <kaleembhattiofficial@gmail.com>
2023-09-29 21:46:59 +0500

    Added the topic router

--- server/app.js
+++ server/app.js
@@ -9,9 +9,10 @@ const express = require("express");
 ////////////////////
 const questionRouter = require("./app/question/router");
 const subjectRouter = require("./app/subject/router");
-const userRouter = require("./app/user/router");
+const topicRouter = require('./app/topic');
+const userRouter = require('./app/user/router');
 
-const { globalErrorHandler } = require("./app/utils/globalErrorHandler");
+const { globalErrorHandler } = require('./app/utils/globalErrorHandler');
 
 ////////////////////
 // Body
@@ -19,17 +20,18 @@ const { globalErrorHandler } = require("./app/utils/globalErrorHandler");
 
 const app = express();
 
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: '10kb' }));
 
 // Middleware
 app.use((req, res, next) => {
-  req.select = "-__v";
+  req.select = '-__v';
   next();
 });
 
 // Routes
-app.use("/api/v1/question", questionRouter);
-app.use("/api/v1/subject", subjectRouter);
+app.use('/api/v1/question', questionRouter);
+app.use('/api/v1/subject', subjectRouter);
+app.use('/api/v1/topic', topicRouter);
 app.use("/api/v1/user", userRouter);
 
 // Global error handler
